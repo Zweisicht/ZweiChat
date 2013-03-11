97b3529cac8b1980103e3999fd5efd00e59912b2
freg <freg@192.168.1.3>
2013-03-11 18:45:44 +0100

    Unnötige Variablen entfernt, Klasse aufgeräumt

--- src/me/zweisicht/zweichat/ZweiModChat.java
+++ src/me/zweisicht/zweichat/ZweiModChat.java
@@ -11,97 +11,86 @@ import org.bukkit.entity.Player;
 
 public class ZweiModChat {
 	
-	private boolean mchatOn = false;
+
 	private Player player;
 
-	
 	String text = "";
-	
-	CommandSender sender;
-	Command cmd;
-	String label;
-	String[] args;
 	
 	ArrayList<String> playerON = new ArrayList<String>();
 	
 	
 
-boolean checkON(String name, String msg){
+	boolean checkON(String name, String msg){
 	
+		boolean ifON = false;
 	
-boolean ifON = false;
-	
-	Server server = Bukkit.getServer();
-	Player[] online = server.getOnlinePlayers();
+		Server server = Bukkit.getServer();
 
-	Player playerCheck;
+		Player playerCheck;
 	
-	//Prüft ob er schreiben darf.
-	if(server.getPlayer(name).hasPermission("zweichat.MODchat") == true){
+		//Prüft ob er schreiben darf.
+		if(server.getPlayer(name).hasPermission("zweichat.MODchat") == true){
 
 			if(playerON.contains(name)){
 			
 				ifON = true;
-				for (int i1=0; i1 < online.length; i1++){
-					playerCheck =  server.getPlayer(online[i1].getName());
+				for (Player player: server.getOnlinePlayers()){
+					
+					playerCheck =  server.getPlayer(player.getName());
 				
-				if (playerCheck.hasPermission("zweichat.MODchat") == true){
-					online[i1].sendMessage(ChatColor.RED + "----<MOD>"+ ChatColor.AQUA + "<" + name + "> " + msg);
+					if (playerCheck.hasPermission("zweichat.MODchat") == true){
+					player.sendMessage(ChatColor.RED + "----<MOD>"+ ChatColor.AQUA + "<" + name + "> " + msg);
+					}
+		
 				}
-		
-	}
 	
-		}
-			
-		
-	}
-	
-	
-	return ifON;
-}
-	
-	void commandQQ(){
-
-				if(playerON.contains(player.getName())){
-					player.sendMessage(ChatColor.RED + "<ZweiChat><MOD> AUS");
-					playerON.remove(playerON.indexOf(player.getName()));	
-					return;
 			}
-		
-			playerON.add(player.getName());
-			player.sendMessage(ChatColor.RED + "<ZweiChat><MOD>"+ ChatColor.GREEN + "AN");
-
+				
 		}
+	
+		return ifON;
+	}
+	//Command /qq
+	void commandQQ(Player player){
+
+		if(playerON.contains(player.getName())){
+			
+			player.sendMessage(ChatColor.RED + "<ZweiChat><MOD> AUS");
+			playerON.remove(playerON.indexOf(player.getName()));	
+			return;
+		}
+		
+		playerON.add(player.getName());
+		player.sendMessage(ChatColor.RED + "<ZweiChat><MOD>"+ ChatColor.GREEN + "AN");
+
+	}
 		
 	
 	//Command /q
 
-	public void commandQ(Player sender, String[] params)
-	  {
+	public void commandQ(Player sender, String[] params){
+		
 		//Parameter abfangen und in textvariable schreiben
 	    for (int i = 0; i < params.length; i++)
 	    {
-	      this.text = (this.text + " " + params[i]);
+	    	this.text = (this.text + " " + params[i]);
 	    }
 	    //Solange Text nicht leer ist
-	    if (!this.text.equals(""))
-	    {// spätere Funktion evtl
-	      if (!this.mchatOn)
-	      {//An jedes Playerobjekt in der Rückgabe von getOnlinePlayers Nachricht schicken
+	    if (!this.text.equals("")){
+	    	
+	    	//An jedes Playerobjekt in der Rückgabe von getOnlinePlayers Nachricht schicken
 	        for (Player player : sender.getServer().getOnlinePlayers())
 	        {
-	          if (player.hasPermission("ZweiChat.mod"))
-	          {
-	            player.sendMessage(ChatColor.RED + "----<MOD>" + ChatColor.AQUA + "<" + sender.getName() + ">" + this.text);
-	          }
+	        	if (player.hasPermission("ZweiChat.MODchat"))
+	        	{
+	        		player.sendMessage(ChatColor.RED + "----<MOD>" + ChatColor.AQUA + "<" + sender.getName() + ">" + this.text);
+	        	}
 
 	        }
-
-	      }
 
 	    }
 
 	    this.text = "";
-	  }
+	}
 	
 }
