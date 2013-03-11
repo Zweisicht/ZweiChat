d28cf2ca67039ff6d7a54d2c8b104d870825e7b7
freg <freg@192.168.1.3>
2013-03-11 20:07:51 +0100

    Command bancheck hinzugefügt

--- src/me/zweisicht/zweichat/ComExec.java
+++ src/me/zweisicht/zweichat/ComExec.java
@@ -11,16 +11,25 @@ public class ComExec implements CommandExecutor {
   private ZweiModChat modChat;
   private ZweiVipChat VIPChat;
   private ZweiIpBlocker IpBlocker;
+  private BanCheck bancheck;
 
-   ComExec(ZweiChat plugin, ZweiModChat modChat, ZweiVipChat VIPChat, ZweiIpBlocker IpBlocker)
+   ComExec(ZweiChat plugin, ZweiModChat modChat, ZweiVipChat VIPChat, ZweiIpBlocker IpBlocker, BanCheck bancheck)
   {
     this.plugin = plugin;
     this.modChat = modChat;
     this.VIPChat = VIPChat;
     this.IpBlocker = IpBlocker;
+    this.bancheck = bancheck;
+
   }
 
-  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] params)
+
+
+
+
+
+
+public boolean onCommand(CommandSender sender, Command cmd, String label, String[] params)
   {
     if ((sender instanceof Player)) {
     	
@@ -51,6 +60,13 @@ public class ComExec implements CommandExecutor {
       			player.sendMessage("Du hast nicht die nötigen Rechte!");
       		}
 
+      		return true;
+      		
+      	case "isbanned":
+
+      			bancheck.IsBanned(player, params);
+      			
+      		
       		return true;
 
       } 
